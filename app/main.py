a4e0563387e787767a58908ac3d1d38e2a417b27
vhsrover <egormetr@gmail.com>
2025-01-13 02:05:39 +0200

    linter_formatter

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
-
+    return {
+        "line": error["line_number"],
+        "colum": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
-
+    return {
+        "errors": [{
+            "line": error["line_number"],
+            "colum": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"
+        } if bool(errors) else [] for error in errors],
+        "path": file_path,
+        "status": "failed" if bool(errors) else "passed"
+    }
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [format_single_linter_file(path, error) for path, error in linter_report.items()]
