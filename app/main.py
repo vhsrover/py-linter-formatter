716085631b60d09da4d85690a03df75f2f49b0ff
vhsrover <egormetr@gmail.com>
2025-01-13 03:13:01 +0200

    edit3

--- app/main.py
+++ app/main.py
@@ -9,16 +9,11 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [{
-            "line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"
-        } if bool(errors) else [] for error in errors],
+        "errors": [{format_linter_error(error)} for error in errors],
         "path": file_path,
         "status": "failed" if bool(errors) else "passed"
     }
+
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(path, error) for path, error in linter_report.items()]
