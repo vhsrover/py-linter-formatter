fbf119b79aede9b06865e46d1d61b62c8da4e906
vhsrover <egormetr@gmail.com>
2025-01-13 02:11:26 +0200

    edit colum-column2

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [{
             "line": error["line_number"],
-            "colum": error["column_number"],
+            "column": error["column_number"],
             "message": error["text"],
             "name": error["code"],
             "source": "flake8"
